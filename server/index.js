ddd186c05046a2fc30c4957638432466f9d3979e
Ali Murtaza <112254545+CodewithAli45@users.noreply.github.com>
2023-06-10 21:54:09 +0530

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 5000;
+const dotenv = require('dotenv');
 const router = require('./routes/taskRoutes');
 const app = express();
 
+const PORT = process.env.PORT || 5000
 // middlewares
 app.use(cors());
 app.use(express.json());
